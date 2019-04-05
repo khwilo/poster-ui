165e2946b1bc4cef0007964f964fb33ec701ba60
khwilo <khwilowatai@gmail.com>
2019-04-05 11:54:06 +0300

    Remove classNames from the elements

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -4,15 +4,9 @@ const Poster = ({ post }) => {
     const {image, title, text} = post;
     return (
         <div className="container">
-            <div className="logoContainer">
-                <img className="logo" src={image} alt="react-logo"/>
-            </div>
-            <div className="titleContainer">
-                <p className="title">{title}</p>
-            </div>
-            <div className="textContainer">
-                <p className="text">{text}</p>
-            </div>
+            <img className="logo" src={image} alt="react-logo"/>
+            <p className="title">{title}</p>
+            <p className="text">{text}</p>
         </div>
     );
 };
